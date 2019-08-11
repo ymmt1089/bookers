385e51597e487073ee4aa0e1f26b389d29e3445c
ymmt1089 <ymmt1089@gmail.com>
2019-08-12 00:04:54 +0900

    [update]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  def new
+  	@book = Book.new
+  end
+
+  def create
+         book = Book.new(book_params)
+         if book.save
+          flash[:notice] = "Book was successfully updated."
+          redirect_to book_path(book.id)
+        else
+          redirect_to book_path(book.id)
+        end
+  end
+
+  def index
+    @book = Book.new
+  	@books = Book.all
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      redirect_to book_path(book.id)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+    private
+    def book_params
+        params.require(:book).permit(:title, :body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  root to: 'home#index'
+
+
 end
